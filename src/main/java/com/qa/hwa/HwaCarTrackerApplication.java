6c20a9962bbe9983abb96b87b5493d23b0ec7d1f
ashkl <asshwin12@yahoo.co.uk>
2021-06-15 00:16:43 +0100

    Initial commit

---
+++ src/main/java/com/qa/hwa/HwaCarTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.qa.hwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwaCarTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwaCarTrackerApplication.class, args);
+	}
+
+}
